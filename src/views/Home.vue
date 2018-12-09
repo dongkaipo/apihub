95bcb997c4c8a1c9cabef70cd08b890058b27c2c
Jack <dongkaipo@126.com>
2018-12-09 22:29:48 +0800

    init
    Signed-off-by: Jack <dongkaipo@126.com>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "../services/http.js";
 
 export default {
   name: "home",
+  data() {
+    return {
+      apiUrl: process.env.VUE_APP_API_URL
+    };
+  },
   components: {
     HelloWorld
+  },
+  mounted() {
+    console.log("i am run");
+    axios({
+      url: "/test/hello",
+      method: "get"
+    })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 };
 </script>
